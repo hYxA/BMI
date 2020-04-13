c943e3c02e2a81a3991c31328960c54cee3a7ef0
Igor Volnukhin <zybu@live.ru>
2020-04-13 23:27:05 +0300

    BmiService. Add system.out.println

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,9 @@
 public class BmiService {
     public float calculate (float weight, int height) {
         float bmi = weight / (height * height);
+
+        System.out.println("At height " + height + " and weight" + weight + " BMI = " + bmi);
+
         return bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        BmiService bmiService = new BmiService();
+
+
+        float bmi = bmiService.calculate(165, 50);
+        float bmi = bmiService.calculate(190, 112);
+        float bmi = bmiService.calculate(165, 60);
+        float bmi = bmiService.calculate(175, 110);
+        float bmi = bmiService.calculate(165, 70);
+        float bmi = bmiService.calculate(155, 50);
 
     }
 }
