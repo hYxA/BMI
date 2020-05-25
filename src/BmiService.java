ad3ac8de09ec657b96c34e8334d24a07bf4434ed
hYxA <54396507+hYxA@users.noreply.github.com>
2020-05-25 22:10:28 +0300

    remove _ from BMI service

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public float calculate (float height, float weight) {
-        float height_m = height / 100;                   // перевод роста в метры
+        float heightmeter = height / 100;                   // перевод роста в метры
         float bmi = weight / (height_m * height_m);
 
         System.out.println("At height " + height + " and weight " + weight + " BMI = " + bmi);
